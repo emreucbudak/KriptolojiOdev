9e89024c611f40758e3efa04bc9f048516d44c9e
gamee <gamee@EMRE>
2025-10-04 18:49:49 +0300

    Proje dosyası ekle.

---
+++ KriptolojiOdev/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace KriptolojiOdev
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            SuspendLayout();
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(621, 113);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(246, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(446, 81);
+            label1.Name = "label1";
+            label1.Size = new Size(169, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Server Bağlantı Durumu:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(650, 81);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 20);
+            label2.TabIndex = 3;
+            label2.Text = "DeAktif";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(446, 116);
+            label3.Name = "label3";
+            label3.Size = new Size(58, 20);
+            label3.TabIndex = 4;
+            label3.Text = "Metin : ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(446, 164);
+            label4.Name = "label4";
+            label4.Size = new Size(49, 20);
+            label4.TabIndex = 5;
+            label4.Text = "Çıktı : ";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(621, 164);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(246, 27);
+            textBox1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(446, 247);
+            button1.Name = "button1";
+            button1.Size = new Size(116, 28);
+            button1.TabIndex = 7;
+            button1.Text = "Bağlantı";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(603, 247);
+            button2.Name = "button2";
+            button2.Size = new Size(116, 28);
+            button2.TabIndex = 8;
+            button2.Text = "Sezar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(751, 247);
+            button3.Name = "button3";
+            button3.Size = new Size(116, 28);
+            button3.TabIndex = 9;
+            button3.Text = "Vigenere";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(446, 298);
+            button4.Name = "button4";
+            button4.Size = new Size(116, 28);
+            button4.TabIndex = 10;
+            button4.Text = "Substitiuion";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(603, 298);
+            button5.Name = "button5";
+            button5.Size = new Size(116, 28);
+            button5.TabIndex = 11;
+            button5.Text = "Tren";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1319, 450);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+    }
+}
