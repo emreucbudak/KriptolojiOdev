d4ea9bd4d365b71d2702c293a56536f6cdeaf942
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-08 17:34:13 +0300

    Sabit keyden kullanıcıdan istenilen keye geçiş yapılıyor

--- KriptolojiOdev/Form1.Designer.cs
+++ KriptolojiOdev/Form1.Designer.cs
@@ -37,11 +37,13 @@
             button4 = new Button();
             button5 = new Button();
             clientLog = new RichTextBox();
+            label1 = new Label();
+            textBox3 = new TextBox();
             SuspendLayout();
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(206, 145);
+            textBox2.Location = new Point(192, 73);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(246, 27);
             textBox2.TabIndex = 1;
@@ -49,7 +51,7 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(49, 145);
+            label3.Location = new Point(49, 80);
             label3.Name = "label3";
             label3.Size = new Size(58, 20);
             label3.TabIndex = 4;
@@ -58,7 +60,7 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(49, 197);
+            label4.Location = new Point(49, 184);
             label4.Name = "label4";
             label4.Size = new Size(49, 20);
             label4.TabIndex = 5;
@@ -66,7 +68,7 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(206, 197);
+            textBox1.Location = new Point(192, 177);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(246, 27);
             textBox1.TabIndex = 6;
@@ -119,11 +121,29 @@
             clientLog.TabIndex = 12;
             clientLog.Text = "";
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(49, 136);
+            label1.Name = "label1";
+            label1.Size = new Size(44, 20);
+            label1.TabIndex = 13;
+            label1.Text = "Key : ";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(192, 133);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(246, 27);
+            textBox3.TabIndex = 14;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1065, 450);
+            Controls.Add(textBox3);
+            Controls.Add(label1);
             Controls.Add(clientLog);
             Controls.Add(button5);
             Controls.Add(button4);
@@ -150,5 +170,7 @@
         private Button button4;
         private Button button5;
         private RichTextBox clientLog;
+        private Label label1;
+        private TextBox textBox3;
     }
 }
